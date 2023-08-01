6906845b21aa01bc50c548b22fa5da9f654959ee
Nova Mondal <rialmondal@gmail.com>
2023-07-31 17:45:52 -0700

    added supporting text and error messages to text fields

--- androidApp/src/main/java/org/team2658/emotion/android/ui/composables/TextInput.kt
+++ androidApp/src/main/java/org/team2658/emotion/android/ui/composables/TextInput.kt
@@ -8,7 +8,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -19,20 +24,78 @@ fun LabelledTextBoxSingleLine(
     label: String,
     text: String,
     modifier: Modifier = Modifier,
-    onValueChange: (String)->Unit,
+    onValueChange: (String) -> Unit,
     keyboardType: KeyboardType = KeyboardType.Text,
-    innerLabel: String = "Enter $label"
+    innerLabel: String = "Enter $label",
+    required: Boolean = true
 ) {
-    Column(modifier = modifier){
+    var showError by remember { mutableStateOf(false) }
+    Column(modifier = modifier) {
 
-        Text(text=label, style = MaterialTheme.typography.labelLarge)
-        Spacer(modifier=Modifier.height(4.dp))
-        OutlinedTextField(value = text, onValueChange = onValueChange,
-            visualTransformation = if(keyboardType==KeyboardType.Password) PasswordVisualTransformation()
-                else VisualTransformation.None,
+        Text(text = label, style = MaterialTheme.typography.labelLarge)
+        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(
+            value = text,
+            onValueChange = onValueChange,
+            modifier = Modifier.onFocusChanged { if (!showError && it.isFocused) showError = true },
+            visualTransformation = if (keyboardType == KeyboardType.Password) PasswordVisualTransformation()
+            else VisualTransformation.None,
             keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
             label = { Text(innerLabel) },
-            singleLine = true
+            singleLine = true,
+            supportingText = {
+                if (required && text.isBlank()) {
+                    Text(text = "* Required")
+                } else if (showError) when (keyboardType) {
+                    KeyboardType.Phone,
+                    KeyboardType.Number,
+                    KeyboardType.NumberPassword -> {
+                        if (text.toIntOrNull() == null) {
+                            Text(
+                                text = "Input must be a whole number",
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                        }
+                    }
+
+                    KeyboardType.Decimal -> {
+                        if (text.toDoubleOrNull() == null) {
+                            Text(
+                                text = "Input must be a decimal number",
+                            )
+                        }
+                    }
+
+                    KeyboardType.Email -> {
+                        if (!text.contains('@')) {
+                            Text(
+                                text = "Input must be a valid email",
+                            )
+                        }
+                    }
+
+                    else -> {}
+                }
+            },
+            isError = showError && when (keyboardType) {
+                KeyboardType.Phone,
+                KeyboardType.Number,
+                KeyboardType.NumberPassword -> {
+                    text.toIntOrNull() == null
+                }
+
+                KeyboardType.Decimal -> {
+                    text.toDoubleOrNull() == null
+                }
+
+                KeyboardType.Email -> {
+                    !text.contains('@')
+                }
+
+                else -> {
+                    required && text.isBlank()
+                }
+            }
         )
 
     }
@@ -43,16 +106,17 @@ fun TextArea(
     label: String,
     text: String,
     modifier: Modifier = Modifier,
-    onValueChange: (String)->Unit,
+    onValueChange: (String) -> Unit,
     keyboardType: KeyboardType = KeyboardType.Text,
     innerLabel: String = "Enter $label"
 ) {
-    Column(modifier = modifier){
+    Column(modifier = modifier) {
 
-        Text(text=label, style = MaterialTheme.typography.labelLarge)
-        Spacer(modifier=Modifier.height(4.dp))
-        OutlinedTextField(value = text, onValueChange = onValueChange,
-            visualTransformation = if(keyboardType==KeyboardType.Password) PasswordVisualTransformation()
+        Text(text = label, style = MaterialTheme.typography.labelLarge)
+        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(
+            value = text, onValueChange = onValueChange,
+            visualTransformation = if (keyboardType == KeyboardType.Password) PasswordVisualTransformation()
             else VisualTransformation.None,
             keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
             label = { Text(innerLabel) },

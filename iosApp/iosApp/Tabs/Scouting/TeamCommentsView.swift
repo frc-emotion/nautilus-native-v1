fb464355cd129b577a26d49deb8344c99e59b353
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-08-09 16:47:29 -0400

    hardcoded TeamSummaryView

---
+++ iosApp/iosApp/Tabs/Scouting/TeamCommentsView.swift
@@ -0,0 +1,21 @@
+//
+//  TeamCommentsView.swift
+//  Σ-Motion
+//
+//  Created by Jason Ballinger on 8/9/23.
+//  Copyright © 2023 team2658. All rights reserved.
+//
+
+import SwiftUI
+
+struct TeamCommentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamCommentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamCommentsView()
+    }
+}

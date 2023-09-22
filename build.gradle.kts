f6cd4feaddaa4bae27ef93ba214deac89bc0b049
Jason Ballinger <jason.ballinger06@gmail.com>
2023-09-22 10:21:44 -0700

    fix linking error by updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,24 @@
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.1.1").apply(false)
-    id("com.android.library").version("8.1.1").apply(false)
-    kotlin("android").version("1.9.0").apply(false)
-    kotlin("multiplatform").version("1.9.0").apply(false)
-    kotlin("plugin.serialization").version("1.9.0").apply(false)
+
+//    id("com.android.application").version("8.1.1").apply(false)
+//    id("com.android.library").version("8.1.1").apply(false)
+//    kotlin("android").version("1.9.0").apply(false)
+//    kotlin("multiplatform").version("1.9.0").apply(false)
+//    kotlin("plugin.serialization").version("1.9.0").apply(false)
+
+    id("com.android.application").version("8.1.0").apply(false)
+    id("com.android.library").version("8.1.0").apply(false)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin").version("2.0.1").apply(false)
+    kotlin("android").version("1.9.10").apply(false)
+    kotlin("multiplatform").version("1.9.10").apply(false)
+    kotlin("plugin.serialization").version("1.9.10").apply(false)
 }
 
 tasks.register("clean", Delete::class) {

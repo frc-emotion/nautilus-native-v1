e84018bb70bf9ae07256794704767b3d9ac0194e
Nova Mondal <rialmondal@gmail.com>
2023-11-15 16:53:25 -0800

    meow

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.team2658.scouting"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.0.0"
+        versionCode = 7
+        versionName = "2.0.2"
     }
     signingConfigs {
         create("release") {

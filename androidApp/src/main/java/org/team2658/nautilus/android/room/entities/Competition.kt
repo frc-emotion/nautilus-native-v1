14c62538a628d63cf0548ab34484ace91cec4e75
Nova Mondal <rialmondal@gmail.com>
2024-01-22 03:17:53 -0800

    remove room dependencies

--- androidApp/src/main/java/org/team2658/nautilus/android/room/entities/Competition.kt
+++
@@ -1,9 +0,0 @@
-package org.team2658.nautilus.android.room.entities
-
-import androidx.room.Entity
-
-@Entity(tableName = "competitions", primaryKeys = ["name", "year"])
-data class Competition(
-    val name: String,
-    val year: String,
-)

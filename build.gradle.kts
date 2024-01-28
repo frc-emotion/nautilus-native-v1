89a8712587d3084dbab35040a06a5841a230af58
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-01-28 13:43:15 -0800

    update gradle + AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.1.1").apply(false)
-    id("com.android.library").version("8.1.1").apply(false)
+    id("com.android.application").version("8.2.2").apply(false)
+    id("com.android.library").version("8.2.2").apply(false)
     id("com.google.devtools.ksp").version("1.9.10-1.0.13").apply(false)
     kotlin("android").version("1.9.10").apply(false)
     kotlin("multiplatform").version("1.9.10").apply(false)

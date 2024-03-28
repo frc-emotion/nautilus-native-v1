c1c080c98f141764b2de5383058e0f90e7eb299d
Nova Mondal <rialmondal@gmail.com>
2024-03-28 16:17:29 -0700

    different sorting for dataview

--- androidApp/src/main/java/org/nautilusapp/nautilus/android/screens/scouting/dataview/ScoutingDataList.kt
+++ androidApp/src/main/java/org/nautilusapp/nautilus/android/screens/scouting/dataview/ScoutingDataList.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,7 @@ import org.nautilusapp.nautilus.android.ui.composables.containers.LazyScreen
 import org.nautilusapp.nautilus.scouting.analysis.average
 import org.nautilusapp.nautilus.scouting.analysis.organize
 import org.nautilusapp.nautilus.scouting.scoutingdata.Crescendo
+import org.nautilusapp.nautilus.toCapitalized
 
 @Composable
 fun ScoutingDataList(
@@ -35,22 +37,38 @@ fun ScoutingDataList(
     onItemClicked: (Pair<Int, Map<Int, List<Crescendo>>>) -> Unit
 ) {
     val organized: Map<Int, Map<Int, List<Crescendo>>> = organize(data)[comp] ?: emptyMap()
-    val sorted = organized
-        .toList()
-        .sortedByDescending { (k, v) ->
-            v
-                .mapValues { (_, v) -> v.average() }
-                .values
-                .toList()
-                .average()
-                .rpTotal
-        }
+//    val sorted = organized
+//        .toList()
+//        .sortedByDescending { (k, v) ->
+//            v
+//                .mapValues { (_, v) -> v.average() }
+//                .values
+//                .toList()
+//                .average()
+//                .rpTotal
+//        }
+    var strat by rememberSaveable { mutableStateOf(SortStrategy.SCORE) }
+
+    val sorted = sortData(organized, strat)
+
 
     LazyScreen(
         onRefresh = onRefresh,
         snack = snack,
         contentPadding = PaddingValues(8.dp)
     ) {
+        item(key = "header") {
+            Column {
+                TextDropDown(
+                    label = "Sort",
+                    value = strat,
+                    items = SortStrategy.entries,
+                    onValueChange = { strat = it }) {
+                    it.name.toCapitalized()
+                }
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+        }
         items(sorted, key = { it.first }) { (teamNumber, data) ->
             val average = data.values.map { it.average() }.average()
             ScoutingDataCard(
@@ -67,6 +85,42 @@ fun ScoutingDataList(
     }
 }
 
+private fun sortData(
+    data: Map<Int, Map<Int, List<Crescendo>>>,
+    sortBy: SortStrategy
+): List<Pair<Int, Map<Int, List<Crescendo>>>> {
+    return data
+        .toList()
+        .sortedByDescending { (k, v) ->
+            val collated = v
+                .mapValues { (_, v) -> v.average() }
+                .values
+                .toList()
+                .average()
+            when (sortBy) {
+                SortStrategy.RP -> collated.rpTotal
+                SortStrategy.AUTO -> collated.auto.ampNotes + collated.auto.speakerNotes
+                SortStrategy.TELEOP -> collated.teleop.speakerAmped + collated.teleop.speakerUnamped + collated.teleop.ampNotes
+                SortStrategy.SPEAKER -> collated.teleop.speakerAmped + collated.teleop.speakerUnamped
+                SortStrategy.AMP -> collated.teleop.ampNotes
+                SortStrategy.SCORE -> collated.score
+                SortStrategy.BREAKDOWN -> collated.breakdownRate
+                SortStrategy.DEFENSIVE -> collated.defensiveRate
+            }
+        }
+}
+
+private enum class SortStrategy {
+    RP,
+    AUTO,
+    TELEOP,
+    SPEAKER,
+    AMP,
+    SCORE,
+    BREAKDOWN,
+    DEFENSIVE
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -76,7 +130,7 @@ fun ScoutingDataListScreen(
     onItemClicked: (Pair<Int, Map<Int, List<Crescendo>>>) -> Unit
 ) {
     val comps = dh.seasons.getComps(2024)
-    var comp by remember { mutableStateOf(comps.firstOrNull()) }
+    var comp by rememberSaveable { mutableStateOf(comps.lastOrNull()) }
     val initData = dh.crescendo.getAll()
     var data by remember { mutableStateOf(initData) }
 
